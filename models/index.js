131a5571e74cf34646d9769bd7a45187483cb4fe
henrylai <e0032134@u.nus.edu>
2022-04-16 15:17:56 +0800

    first commit

--- models/index.js
+++ models/index.js
@@ -1,7 +1,27 @@
 const bookshelf = require('../bookshelf')
 
 const Product = bookshelf.model('Product', {
-    tableName:'products'
+    tableName:'products',
+    category() {
+        return this.belongsTo('Category');
+    },
+    tags(){
+        return this.belongsToMany('Tag');
+    }
 });
 
-module.exports = { Product };
+const Category = bookshelf.model('Category',{
+    tableName: 'categories',
+    category() {
+        return this.hasMany('Product', 'category_id');
+    }
+})
+
+const Tag = bookshelf.model('Tag',{
+    tableName: 'tags',
+    products() {
+        return this.belongsToMany('Product')
+    }
+})
+
+module.exports = { Product, Category, Tag};
--- routes/products.js
+++ routes/products.js
@@ -2,14 +2,175 @@ const express = require("express");
 const router = express.Router();
 
 // #1 import in the Product model
-const {Product} = require('../models')
+const { Product, Category, Tag } = require('../models')
 
-router.get('/', async (req,res)=>{
+// import in the Forms
+const { bootstrapField, createProductForm } = require('../forms');
+
+router.get('/', async (req, res) => {
     // #2 - fetch all the products (ie, SELECT * from products)
-    let products = await Product.collection().fetch();
+    let products = await Product.collection().fetch({
+        withRelated:['category']
+    });
     res.render('products/index', {
         'products': products.toJSON() // #3 - convert collection to JSON
     })
+})
+
+router.get('/create', async (req, res) => {
+
+    const allCategories = await Category.fetchAll().map((category)=>{
+        return [category.get('id'), category.get('name')];
+    })
+
+    const allTags = await Tag.fetchAll().map( tag => [tag.get('id'), tag.get('name')]);
+
+    const productForm = createProductForm(allCategories, allTags);
+    res.render('products/create', {
+        'form': productForm.toHTML(bootstrapField)
+    })
+})
+
+
+router.post('/create', async (req, res) => {
+
+    const allCategories = await Category.fetchAll().map((category) => {
+        return [category.get('id'), category.get('name')];
+    })
+    const productForm = createProductForm(allCategories);
+    productForm.handle(req, {
+        'success': async (form) => {
+            //separate out tags from the other product data
+            //as not to cause an error when we create 
+            // the new product
+            let {tags, ...productData} = form.data;
+            const product = new Product(productData);
+            // product.set('name', form.data.name);
+            // product.set('cost', form.data.cost);
+            // product.set('description', form.data.description);
+            await product.save();
+            if(tags) {
+                await product.tags().attach(tags.split(","));
+            }
+            res.redirect('/products');
+        },
+        'error': async (form) => {
+            res.render('products/create', {
+                'form': form.toHTML(bootstrapField)
+            })
+        }
+
+    })
+})
+
+
+router.get('/:product_id/update', async (req, res) => {
+    // retrieve the product
+    const productId = req.params.product_id
+    const product = await Product.where({
+        'id': productId
+    }).fetch({
+        require: true,
+        withRelated:['tags']
+    });
+    //fetch all the tags
+    const allTags = await Tag.fetchAll().map( tag => [tag.get('id'), tag.get('name')]);
+
+
+    // fetch all the categories
+    const allCategories = await Category.fetchAll().map((category)=>{
+        return [category.get('id'), category.get('name')];
+    })
+
+    const productForm = createProductForm(allCategories, allTags);
+
+    // fill in the existing values
+    productForm.fields.name.value = product.get('name');
+    productForm.fields.cost.value = product.get('cost');
+    productForm.fields.description.value = product.get('description');
+    productForm.fields.category_id.value = product.get('category_id');
+
+    //fill in the muti-select for the tags
+    let selectedTags = await product.related('tags').pluck('id');
+    productForm.fields.tags.value = selectedTags;
+
+    res.render('products/update', {
+        'form': productForm.toHTML(bootstrapField),
+        'product': product.toJSON()
+    })
+
+})
+
+router.post('/:product_id/update', async (req, res) => {
+
+    //fetch all the categories
+    const allCategories = await (await Category.fetchAll()).invokeMap((category)=>{
+        return [category.get('id'), category.get('name')];
+    })
+
+    // fetch the product that we want to update
+    const product = await Product.where({
+        'id': req.params.product_id
+    }).fetch({
+        require: true
+    });
+
+    // process the form
+    const productForm = createProductForm(allCategories);
+    productForm.handle(req, {
+        'success': async (form) => {
+            let {tags, ...productData} = form.data
+            product.set(productData);
+            product.save();
+
+            let tagIds = tags.split(',');
+            let existingTagIds = await product.related('tags').pluck('id');
+
+            // remove all the tags that aren't selected anymore
+            let toRemove = existingTagIds.filter( id => tagIds.includes(id) === false);
+            await product.tags().detach(toRemove);
+
+            // add in all the tags selected in the form
+            await product.tags().attach(tagIds);
+
+
+            res.redirect('/products');
+        },
+        'error': async (form) => {
+            res.render('products/update', {
+                'form': form.toHTML(bootstrapField),
+                'product': product.toJSON()
+            })
+        }
+    })
+
+})
+
+
+router.get('/:product_id/delete', async(req,res)=>{
+    // fetch the product that we want to delete
+    const product = await Product.where({
+        'id': req.params.product_id
+    }).fetch({
+        require: true
+    });
+
+    res.render('products/delete', {
+        'product': product.toJSON()
+    })
+
+});
+
+
+router.post('/:product_id/delete', async(req,res)=>{
+    // fetch the product that we want to delete
+    const product = await Product.where({
+        'id': req.params.product_id
+    }).fetch({
+        require: true
+    });
+    await product.destroy();
+    res.redirect('/products')
 })
 
 module.exports = router;
