4257375a2f98ed41c6df61c3e7c99e64d2b71370
henrylai <e0032134@u.nus.edu>
2022-04-17 23:29:36 +0800

    added tag to the UI

--- routes/products.js
+++ routes/products.js
@@ -7,10 +7,38 @@ const { Product, Category, Tag } = require('../models')
 // import in the Forms
 const { bootstrapField, createProductForm } = require('../forms');
 
+
+async function getProductById(productId){
+    // eqv of
+    // select * from products where id = ${productId}
+    const product = await Product.where({
+        'id': productId
+    }).fetch({
+        'require': true, // will cause an error if not found
+        'withRelated':['category', 'tags']  // load in the associated category and tags
+    })
+    return product;
+
+}
+
+async function getAllCategories() {
+    const allCategories = await Category.fetchAll().map( category => {
+        return [ category.get('id'), category.get('name')]
+    });
+    return allCategories;
+}
+
+async function getAllTags() {
+    const allTags = await Tag.fetchAll().map( tag => {
+        return [ tag.get('id'), tag.get('name')]
+    })
+    return allTags;
+}
+
 router.get('/', async (req, res) => {
     // #2 - fetch all the products (ie, SELECT * from products)
     let products = await Product.collection().fetch({
-        withRelated:['category']
+        withRelated:['category', 'tags']
     });
     res.render('products/index', {
         'products': products.toJSON() // #3 - convert collection to JSON
@@ -71,7 +99,7 @@ router.get('/:product_id/update', async (req, res) => {
         'id': productId
     }).fetch({
         require: true,
-        withRelated:['tags']
+        withRelated:['category','tags']
     });
     //fetch all the tags
     const allTags = await Tag.fetchAll().map( tag => [tag.get('id'), tag.get('name')]);
@@ -113,7 +141,7 @@ router.post('/:product_id/update', async (req, res) => {
         'id': req.params.product_id
     }).fetch({
         require: true,
-        withRelated:['tags']
+        withRelated:['category','tags']
     });
 
     // process the form
