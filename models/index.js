970cac3a5e1a8fc48ec925f549ca9c590c0712e6
henrylai <e0032134@u.nus.edu>
2022-04-18 22:55:24 +0800

    complete check point 16

--- models/index.js
+++ models/index.js
@@ -25,7 +25,9 @@ const Tag = bookshelf.model('Tag',{
     }
 })
 
+const User = bookshelf.model('User',{
+    tableName: 'users'
+})
 
 
-
-module.exports = { Product, Category, Tag};
+module.exports = { Product, Category, Tag, User};
